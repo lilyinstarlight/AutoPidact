2d038627fab5a8559ee5ddfc7555eb99951352be
Foster McLane <fkmclane@gmail.com>
2014-05-03 14:36:40 -0400

    Added simple setup

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
+from distutils.core import setup
+
+setup(	name='AutoPidact',
+	version=0.1,
+	description='A visual learning program designed for the Raspberry Pi',
+	author='Foster McLane',
+	author_email='fkmclane@gmail.com',
+	packages=['autopidact'],
+	scripts=['bin/autopidact'])
