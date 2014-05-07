a1fdf75b3616f59a0d295a2711c84bfc727f4fb5
Foster McLane <fkmclane@gmail.com>
2014-05-07 16:08:51 -0400

    Fix timeout interval

--- autopidact/view.py
+++ autopidact/view.py
@@ -2,7 +2,7 @@ from gi.repository import Gtk, GdkPixbuf, GLib
 import cv2
 
 class View(Gtk.Window):
-	def __init__(self, title, camera, interval=200):
+	def __init__(self, title, camera, interval=2000):
 		Gtk.Window.__init__(self)
 		self.set_title(title)
 		self.set_size_request(640, 480)
@@ -15,7 +15,9 @@ class View(Gtk.Window):
 	def update(self):
 		if self.cam.isReady():
 			frame = cv2.cvtColor(self.cam.getFrame(), cv2.COLOR_BGR2RGB)
-			pixbuf = GdkPixbuf.Pixbuf.new_from_data(bytes(frame), GdkPixbuf.Colorspace.RGB, False, 8, frame.shape[1], frame.shape[0], frame.strides[1], None, None)
+			pixbuf = GdkPixbuf.Pixbuf.new_from_data(bytes(frame), GdkPixbuf.Colorspace.RGB, False, 8, frame.shape[1], frame.shape[0], frame.strides[0], None, None)
 			self.img.set_from_pixbuf(pixbuf)
 		else:
 			print('Camera not ready')
+
+		return True
